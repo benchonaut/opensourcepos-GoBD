33f399edf541d8ff63a565e055eceae5eb2fffee
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-01-17 13:35:10 +0100

    vnc display detection 0.1

--- ospos_user_scripts/report-daily.sh
+++ ospos_user_scripts/report-daily.sh
@@ -3,4 +3,4 @@ vnc_display_first() { ps -Fc|grep Xtight|grep -v grep |grep X11|sed 's/.\+Xtight
 osposurl="http://127.0.0.1/ospos/"
 outdir=~/pdfout
 target=$osposurl"public/reports/detailed_sales/"$(date +%F)"/"$(date +%F)"/all/all"
-test -e $target || firefox --display $(vnc_display_first )-print-file $outdir/reports/$(date +%F).pdf -print $target -print-mode pdf -print-header no -print-footer no -print-delay 2
+test -e $target || firefox --display $(vnc_display_first ) -print-file $outdir/reports/$(date +%F).pdf -print $target -print-mode pdf -print-header no -print-footer no -print-delay 2
